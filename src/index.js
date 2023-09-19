17e0d4fbcc27fa4a72cb644b43814bad0b6396cf
webtoma <thomas.delalbre@gmail.com>
2023-09-19 14:56:17 +0200

    adding bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
-
+import '../src/scss/styles.scss' //Import scss
+import * as bootstrap from 'bootstrap' //Bootstrap JS
 function component() {
     const element = document.createElement('div');
+    element.classList.add('container');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
