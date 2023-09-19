1c36d51550f410980e3f6d71a4b7121ca518d9e2
webtoma <thomas.delalbre@gmail.com>
2023-09-19 15:58:08 +0200

    ESLint added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import _ from 'lodash';
-import '../src/scss/styles.scss' //Import scss
-import * as bootstrap from 'bootstrap' //Bootstrap JS
-function component() {
-    const element = document.createElement('div');
-    element.classList.add('container');
-    element.innerHTML = _.join(['Hello', 'OnePoint'], ' ');
-    return element;
+import _ from 'lodash'
+import '../src/scss/styles.scss' // Import scss
+function component () {
+  const element = document.createElement('div')
+  element.classList.add('container')
+  element.innerHTML = _.join(['Hello', 'OnePoint'], ' ')
+  return element
 }
-document.body.appendChild(component());
+document.body.appendChild(component())
